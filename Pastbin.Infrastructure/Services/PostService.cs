df18d95d7e0d43162a5189020a4e2f172e110909
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-07-13 20:32:27 +0500

    ыы

--- Pastbin.Infrastructure/Services/PostService.cs
+++ Pastbin.Infrastructure/Services/PostService.cs
@@ -1,65 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Pastbin.Application.Interfaces;
+﻿using Pastbin.Application.Interfaces;
 using Pastbin.Application.Services;
 using Pastbin.Domain.Entities;
-using Pastbin.Infrastructure.DataAccess;
-using System.Text;
 namespace Pastbin.Infrastructure.Services
 {
     public class PostService : IPostService
     {
-
-        private readonly IFileService _fileService;
-        private readonly PastbinDbContext _db;
-        public PostService(IFileService fileService, PastbinDbContext db)
+        public Task<Post> CreateAsync(Post entity,string Text)
         {
-            _db = db;
-            _fileService = fileService;
+            
         }
-        public async Task<Post> CreateAsync(Post entity, string text)
-        {
-            // Создание MemoryStream из текста
-            byte[] byteArray = Encoding.UTF8.GetBytes(text);
-            using (MemoryStream memoryStream = new MemoryStream(byteArray))
-            {
-                // Использование MemoryStream для загрузки файла
-                var response = await _fileService.UploadFileAsync("shokir-demo-bucket", memoryStream, "file.txt", entity.ExpireHour, null);
-
-                // Обновление сущности
-                entity.UrlAWS = response.UploadedFilePath;
-                entity.HashUrl = string.Join("", HashGenerator.sha256_hash(response.UploadedFilePath).Select(x => x).Take(40));
-            }
-            await _db.Posts.AddAsync(entity);
-            await _db.SaveChangesAsync();
-            return entity;
-        }
-        public Task<bool> DeleteAsync(int id)
+        public Task<bool> DeleteAsync(int Id)
         {
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<Post>> GetAllFromUsernameAsync(string username)
+        public Task<IEnumerable<Post>> GetAllAsync()
         {
-            var user = await _db.Users
-            .Include(u => u.Posts) 
-            .FirstOrDefaultAsync(u => u.Username == username);
-
-            if (user == null) return new List<Post>(); 
-
-            return user.Posts;
-        }
-        public async Task<IEnumerable<Post>> GetAllAsync()
-        {
-            var Posts = _db.Posts.ToList();
-            return Posts;
+            throw new NotImplementedException();
         }
 
-        public async Task<Post> GetByIdAsync(int id)
+        public Task<Post> GetByIdAsync(int id)
         {
-            return await _db.Posts.FirstOrDefaultAsync(x => x.Id == id);
+            throw new NotImplementedException();
         }
 
-        public Task<Post> UpdateAsync(Post post)
+        public Task<Post> UpdateAsync(int Id)
         {
             throw new NotImplementedException();
         }
